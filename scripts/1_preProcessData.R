a200c174496c8a5ebedcc7538cd0b90b9b6029c3
dyammons <dyammons95@gmail.com>
2024-03-04 08:19:31 -0700

    Update paths to make revised structure

--- scripts/1_preProcessData.R
+++ scripts/1_preProcessData.R
@@ -8,13 +8,13 @@ outName <- "tils_naive6_w_all_OS_PBMC"
 subName <- "allCells"
 
 #load in annotated tumor data aligned using cellranger 6.1.2
-seu.obj.tumor <- readRDS(file = "./output/s3/canine_naive_n6_annotated.rds")
+seu.obj.tumor <- readRDS(file = "../input/canine_naive_n6_annotated.rds")
 
 #retrieve object aligned using cellranger 6.1.2
-seu.obj.pbmc <- readRDS(file = "/pl/active/dow_lab/dylan/k9_PBMC_scRNA/analysis/output/s3/220926_cfam_hVoWadj_res1.2_dims45_S3.rds")
+seu.obj.pbmc <- readRDS(file = "../input/220926_cfam_hVoWadj_res1.2_dims45_S3.rds")
 
 #load in annotated object associated with the primary publication
-reference <- readRDS(file = "../../k9_PBMC_scRNA/analysis/output/s3/final_dataSet_HvO.rds")
+reference <- readRDS(file = "../input/final_dataSet_HvO.rds")
 
 #correct barcode discprencies to enable transfer of cell type labels
 rownames(reference@meta.data) <- ifelse(grepl("_8", rownames(reference@meta.data)),paste0(substr(rownames(reference@meta.data), 1, nchar(rownames(reference@meta.data))-2),"_111") ,rownames(reference@meta.data))
@@ -40,7 +40,7 @@ seu.obj.tils <- subset(seu.obj.tumor,
                                      "T_cycling","cDC1","cDC2","mregDC","pDC","preDC")
                          )
 
-seu.obj.tils <- dataVisUMAP(seu.obj = seu.obj.tils, outDir = "./output/s3/", outName = "20231014_tils_2500", final.dims = 40, final.res = 0.6, stashID = "clusterID_2", 
+seu.obj.tils <- dataVisUMAP(seu.obj = seu.obj.tils, outDir = "../output/s3/", outName = "20231014_tils_2500", final.dims = 40, final.res = 0.6, stashID = "clusterID_2", 
                         algorithm = 3, prefix = "integrated_snn_res.", min.dist = 0.6, n.neighbors = 75, assay = "integrated", saveRDS = F,
                         features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
                                      "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
@@ -48,7 +48,7 @@ seu.obj.tils <- dataVisUMAP(seu.obj = seu.obj.tils, outDir = "./output/s3/", out
                        )
 
 
-### Fig 1a: create raw UMAP of tumor cells used in analysis
+### Fig 1a-1: create raw UMAP of tumor cells used in analysis
 pi <- DimPlot(seu.obj.tils, 
               reduction = "umap", 
               group.by = "celltype.l1",
@@ -62,14 +62,14 @@ pi <- formatUMAP(plot = pi) + NoLegend() + theme(axis.title = element_blank(),
                                                  panel.border = element_blank(), 
                                                  plot.margin = unit(c(0, 0, 0, 0), "cm"),
                                                 plot.title = element_text(size= 26)) + ggtitle("Tumor infiltrating immune cells (TILs)")
-ggsave(paste("./output/", outName,"/",subName, "/tils_rawUMAP.png", sep = ""), width = 7, height = 7)
+ggsave(paste("../output/", outName,"/",subName, "/tils_rawUMAP.png", sep = ""), width = 7, height = 7)
 
 #select cells to integrate from pbmcs (all pbmcs from OS dogs)
 seu.obj.pbmc.os <- subset(seu.obj.pbmc, 
                           subset = cellSource == "Osteosarcoma"
                          )
 
-seu.obj.pbmc.os <- dataVisUMAP(seu.obj = seu.obj.pbmc.os, outDir = "./output/s3/", outName = "20231014_pbmc_os_2500", final.dims = 40, final.res = 0.2, stashID = "clusterID_2", 
+seu.obj.pbmc.os <- dataVisUMAP(seu.obj = seu.obj.pbmc.os, outDir = "../output/s3/", outName = "20231014_pbmc_os_2500", final.dims = 40, final.res = 0.2, stashID = "clusterID_2", 
                         algorithm = 3, prefix = "integrated_snn_res.", min.dist = 0.6, n.neighbors = 75, assay = "integrated", saveRDS = F,
                         features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
                                      "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
@@ -77,7 +77,7 @@ seu.obj.pbmc.os <- dataVisUMAP(seu.obj = seu.obj.pbmc.os, outDir = "./output/s3/
                        )
 
 
-### Fig 1a: create raw UMAP of pbmcs used in analysis
+### Fig 1a-2: create raw UMAP of pbmcs used in analysis
 pi <- DimPlot(seu.obj.pbmc.os, 
               reduction = "umap", 
               group.by = "clusterID_2",
@@ -90,17 +90,17 @@ pi <- formatUMAP(plot = pi) + NoLegend() + theme(axis.title = element_blank(),
                                                  panel.border = element_blank(), 
                                                  plot.margin = unit(c(0, 0, 0, 0), "cm"),
                                                 plot.title = element_text(size= 26)) + ggtitle("Circulating immune cells")
-ggsave(paste("./output/", outName,"/",subName, "/pbmcOS_rawUMAP.png", sep = ""), width = 7, height = 7)
+ggsave(paste("../output/", outName,"/",subName, "/pbmcOS_rawUMAP.png", sep = ""), width = 7, height = 7)
 
 
 #integrate the data
 seu.sub.list <- c(SplitObject(seu.obj.pbmc.os, split.by = "orig.ident"),SplitObject(seu.obj.tils, split.by = "name"))
-seu.obj <- indReClus(seu.obj = NULL, outDir = "./output/s2/", subName = "bloodANDtils", preSub = T, seu.list = seu.sub.list, nfeatures = 2500, vars.to.regress = "percent.mt")
+seu.obj <- indReClus(seu.obj = NULL, outDir = "../output/s2/", subName = "bloodANDtils", preSub = T, seu.list = seu.sub.list, nfeatures = 2500, vars.to.regress = "percent.mt")
 
-# seu.obj <- readRDS(file = "./output/s2/bloodANDtils_S2.rds")
-clusTree(seu.obj = seu.obj, dout = "./output/clustree/", outName = "bloodANDtils", test_dims = c(50,45,40,35,30,25), algorithm = 3, prefix = "integrated_snn_res.")
+# seu.obj <- readRDS(file = "../output/s2/bloodANDtils_S2.rds")
+clusTree(seu.obj = seu.obj, dout = "../output/clustree/", outName = "bloodANDtils", test_dims = c(50,45,40,35,30,25), algorithm = 3, prefix = "integrated_snn_res.")
 
-seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "./output/s3/", outName = "20230505_bloodANDtils_2500", final.dims = 40, final.res = 0.6, stashID = "clusterID_2", 
+seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = "20230505_bloodANDtils_2500", final.dims = 40, final.res = 0.6, stashID = "clusterID_2", 
                         algorithm = 3, prefix = "integrated_snn_res.", min.dist = 0.6, n.neighbors = 75, assay = "integrated", saveRDS = F,
                         features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
                                      "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
@@ -115,11 +115,11 @@ seu.obj.sub$clusterID_2 <- NULL
 seu.obj.sub$clusterID <- NULL
 
 #repeat integration with pal conaminated cells removed
-seu.obj <- indReClus(seu.obj = seu.obj.sub, outDir = "./output/s2/", subName = "20230505_bloodANDtils_QCfiltered_2500", preSub = T, nfeatures = 2500,
+seu.obj <- indReClus(seu.obj = seu.obj.sub, outDir = "../output/s2/", subName = "20230505_bloodANDtils_QCfiltered_2500", preSub = T, nfeatures = 2500,
                       vars.to.regress = "percent.mt"
                        )
 
-seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "./output/s3/", outName = "20230505_bloodANDtils_QCfiltered_2500_QCfiltered_2500", final.dims = 40, final.res = 1.2, stashID = "clusterID", 
+seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = "20230505_bloodANDtils_QCfiltered_2500_QCfiltered_2500", final.dims = 40, final.res = 1.2, stashID = "clusterID", 
                         algorithm = 3, prefix = "integrated_snn_res.", min.dist = 0.5, n.neighbors = 60, assay = "integrated", saveRDS = F,
                         features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
                                      "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
@@ -128,4 +128,4 @@ seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "./output/s3/", outName = "20
 
 #clean metadata a bit and save the object for down stream use
 seu.obj@meta.data <- seu.obj@meta.data[,!grepl("DF|pANN", colnames(seu.obj@meta.data))]
-saveRDS(seu.obj, "./output/s3/20230505_bloodANDtils_QCfiltered_2500_QCfiltered_2500_res1.2_dims40_dist0.5_neigh60_S3.rds")
+saveRDS(seu.obj, "../output/s3/20230505_bloodANDtils_QCfiltered_2500_QCfiltered_2500_res1.2_dims40_dist0.5_neigh60_S3.rds")
