5ed4579980c9abdd926b5cdc0dc72788624af9f6
dyammons <dyammons95@gmail.com>
2024-03-08 09:09:43 -0700

    Update monocyte analysis for pub

--- scripts/7_monocytes_analysis.R
+++ scripts/7_monocytes_analysis.R
@@ -86,7 +86,7 @@ vilnPlots(seu.obj = seu.obj, groupBy = "clusID_new", numOfFeats = 24, outName = 
                      outDir = paste0("../output/viln/", outName, "/"), outputGeneList = T, filterOutFeats = c("^MT-", "^RPL", "^ENSCAF", "^RPS")
                     )
 
-### Fig 5a - UMAP by clusterID_sub
+### Fig 6a - UMAP by clusterID_sub
 pi <- DimPlot(seu.obj, 
               reduction = "umap", 
               group.by = "clusID_new",
@@ -102,12 +102,12 @@ pi <- cusLabels(plot = pi, shape = 21, size = 10, textSize = 6,
 ggsave(paste("../output/", outName, "/", "rawUMAP.png", sep = ""), width = 7, height = 7)
 
 
-### Fig 5b - skew plot for abundance analysis
+### Fig 6b - skew plot for abundance analysis
 p <- skewPlot(seu.obj, groupBy = "majorID_sub", outDir = paste0("../output/", outName), outName = outName)
 ggsave(paste0("../output/", outName, "/", "skewPlot.png"), width = 6, height = 4)
 
 
-### Fig 5c - DGE analysis
+### Fig 6c - DGE analysis
 
 #load in the tumor and pal signatures to exlude from DE analysis
 pal_feats = c('TIMP1', 'NAA10', 'ENSCAFG00000037735', 'GP6', 'SEC11C', 'FTL', 'NRGN', 'ACOT7', 'VCL', 'RSU1', 'ITGB1', 'H3-3A', 'RABGAP1L', 'SELP', 'SH3GLB1', 'ACTB', 'ENSCAFG00000008221', 'TLN1', 'GSN', 'AMD1', 'TREM2', 'SH3BGRL2', 'MYH9', 'PLEK', 'ENSCAFG00000042554', 'RAP1B', 'ENSCAFG00000004260', 'NAP1L1', 'PPBP', 'RASA3', 'ITGA2B', 'EIF1', 'ACTG1', 'C9H17orf64', 'JMJD6', 'CCL14', 'GNG11', 'IGF2BP3', 'TBXAS1', 'VDAC3', 'MARCHF2', 'TPM4', 'TKT', 'FTH1.1', 'FERMT3', 'RTN3', 'PRKAR2B', 'SVIP', 'ENSCAFG00000030286', 'ADA', 'MYL9', 'TUBB1', 'TUBA1B', 'METTL7A', 'THBS1', 'SERF2', 'PIF1', 'B2M', 'GAS2L1', 'YWHAH', 'HPSE', 'ATG3', 'ENSCAFG00000015217', 'ITGA6','RGS18', 'SUB1', 'LGALS1', 'CFL1', 'BIN2', 'CAT', 'RGS10', 'MGST3', 'TMBIM6', 'PFN1', 'CD63', 'RALBP1', 'GNAS', 'SEPTIN7', 'TPT1', 'UBB', 'ATF4', 'BBLN', 'MTDH', 'ENSCAFG00000017655','FYB1', 'ENO1', 'GABARAP', 'SSR4', 'MSN', 'ENSCAFG00000011134', 'ENSCAFG00000046637', 'COX8A', 'DLA-64', 'CD47', 'VASP', 'DYNLRB1', 'DLA88', 'SMDT1', 'ATP5PF','ELOB', 'ENSCAFG00000029155', 'ARPC3', 'VPS28', 'LRRFIP1', 'SRP14', 'ABRACL', 'ENSCAFG00000043577', 'ENSCAFG00000042598')
@@ -128,18 +128,18 @@ p <- prettyVolc(plot = p_volc[[1]], rightLab = NULL, leftLab = NULL, rightCol = 
 ggsave(paste0("../output/", outName, "/", "volcPlot.png"), width = 7, height = 7)
 
 
-### Fig 5d - GO GSEA of DEGs
+### Fig 6d - GO GSEA of DEGs
 p <- plotGSEA(pwdTOgeneList = paste0("../output/", outName, "/pseudoBulk/allCells/", outName, "_cluster_allCells_all_genes.csv"),
               geneList = NULL, category = "C5", species = "dog", termsTOplot = 10, upOnly = T, trunkTerm = T,
               pvalueCutoff = 0.05, subcategory = NULL, 
               saveRes = paste0("../output/", outName, "/c5_", outName, "_res.csv")) + theme(axis.title=element_text(size = 16))
-p <- p + scale_x_continuous(limits = c(-25,ceiling(max(p$data$x_axis)*1.05)), 
+p <- p + scale_x_continuous(limits = c(-20,ceiling(max(p$data$x_axis)*1.05)), 
                             breaks = c(0,ceiling(max(p$data$x_axis)*1.05)/2,ceiling(max(p$data$x_axis)*1.05)),
                             name = "log10(p.adj)") + ggtitle("Gene ontology") + theme(plot.title = element_text(size = 20, hjust = 0.5))
 ggsave(paste0("../output/", outName, "/", "gseaPlot_1.png"), width = 7, height = 7)
 
 
-### Fig 5e - Reactome GSEA of DEGs
+### Fig 6e - Reactome GSEA of DEGs
 p <- plotGSEA(pwdTOgeneList = paste0("../output/", outName, "/pseudoBulk/allCells/", outName, "_cluster_allCells_all_genes.csv"),
          geneList = NULL, category = "C2", species = "dog", termsTOplot = 10, upOnly = T, trunkTerm = T,
                      pvalueCutoff = 0.05, subcategory = "CP:REACTOME", saveRes = paste0("../output/", outName, "/c2_", outName, "_res.csv")
@@ -148,7 +148,7 @@ p <- p + scale_x_continuous(limits = c(-20,ceiling(max(p$data$x_axis)*1.05)), br
 ggsave(paste0("../output/", outName, "/", "gseaPlot_2.png"), width = 7, height = 7)
 
 
-### Fig 5f - Split UMAP of selected DEGs
+### Fig 6f - Split UMAP of selected DEGs
 Idents(seu.obj) <- "cellSource"
 seu.obj.sub <- subset(x = seu.obj, downsample = min(table(seu.obj$cellSource)))
 
